8423ef98190a64cc98bf9fb6712c649ce9fd603f
TroyChen <37316904+TroyChen01@users.noreply.github.com>
2019-01-22 20:12:11 +0800

    Add files via upload

---
+++ Practice Exercises for Expressions.py
@@ -0,0 +1,8 @@
+print("13 miles are",5280*13, "feets")
+print("the number of seconds in 77 hours, 2121 minutes and 3737 seconds are", 77*60*60+2121*60+3737)
+print("the length in inches of the perimeter of a rectangle with sides of length 44 and 77 inches is", 2*44+2*77)
+print("the area in square inches of a rectangle with sides of length 44 and 77 inches is", 44*77)
+print("the value of 10001000 dollars compounded at 77 percent interest for 1010 years is", int(1000*(1+77*0.01)**10))
+print("John Warren is", str(52), "years old")
+print("the distance between the points (2, 2)(2,2) and (5, 6)(5,6)", ((6-2)**2+(5-2)**2)**0.5)
+
